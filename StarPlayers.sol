37cec44f8993893de1fc759b10a8da5c8854fc41
Vinicius <vinicius190702@hotmail.com>
2024-10-09 16:07:53 -0300

    Adding multiple NFTs to the collection

--- StarPlayers.sol
+++ StarPlayers.sol
@@ -1,74 +1,137 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract StarPlayers is ERC721URIStorage, ERC721Enumerable, Ownable {
-    uint256 public tokenCounter; // Contador de NFT criados
-    string public contractURI = "https://green-total-felidae-639.mypinata.cloud/ipfs/QmZANaEXzD65hfABwRMJCA52m4injWbqHER9jX4QeqBcyt";  // Variável para armazenar o URI do contrato
-    string public nftTokenURI = "https://green-total-felidae-639.mypinata.cloud/ipfs/QmWGTAoNLL7a2WDGQBqUMkPV6mS6hrkAMRNTr21fa9i4S4";
-    uint16 public supply = 200;
-    uint256 public mintPrice = 0.05 ether;
-    uint16 public royaltyFee = 500;
-    address public royaltyRecipient = owner();
-
-    mapping(address => uint256) public addressMintedBalance;
-    uint256 public maxMintAmount = 5;
-
-    event NFTCreated(address indexed to, uint256 indexed tokenId);
-
-    constructor() ERC721("StarPlayers", "STAR") Ownable(msg.sender) {
-        tokenCounter = 0;
+contract StarPlayers is ERC721, Ownable {
+    struct NFTType {
+        uint8 currentSupply;
+        uint8 maxSupply;
+        uint256 mintPrice;
+        string baseURI;
     }
 
-    function createNFT(address to, string memory tokenURI) public payable {
-        // Arrumar os erros
-        require(addressMintedBalance[to] < maxMintAmount, "Mint limit reached for this address");
-        require(msg.value >= mintPrice, "Insuficient value to mint the NFT");
-        require(tokenCounter < supply, "All NFTs minteds");
+    mapping(uint8 => NFTType) public nftTypes;
+    mapping(uint8 => uint8) public tokenType;
 
-        tokenCounter += 1;
-        uint256 newTokenId = tokenCounter;
-        _safeMint(to, newTokenId);
-        _setTokenURI(newTokenId, nftTokenURI);
-        addressMintedBalance[to] += 1;
+    uint8 public nextTokenId;
 
-        emit NFTCreated(to, newTokenId);
+    error SupplyExceeded(uint8 typeId);
+    error InsufficientValue(uint256 required, uint256 sent);
+    error FailedToFindAValidType();
+
+    event NFTCreated(uint8 indexed tokenId, uint8 indexed typeId, address indexed to);
+
+    constructor() ERC721("StarPlayers", "STAR") Ownable(msg.sender) {
+        nftTypes[1] = NFTType({
+            currentSupply: 0,
+            maxSupply: 10,
+            mintPrice: 0.01 ether,
+            baseURI: "https://gateway.pinata.cloud/ipfs/QmQK2n27R2Hisi9QQcoso2sMM8i2Tjyn6d5LZwKs8aXHkF/"
+        });
+        nftTypes[2] = NFTType({
+            currentSupply: 0,
+            maxSupply: 30,
+            mintPrice: 0.007 ether,
+            baseURI: "https://gateway.pinata.cloud/ipfs/QmQVCBEMCwRHNC9F4wfGYutacJnxPi8Q1cPtuRoJpnc37B/"
+        });
+        nftTypes[3] = NFTType({
+            currentSupply: 0,
+            maxSupply: 50,
+            mintPrice: 0.001 ether,
+            baseURI: "https://gateway.pinata.cloud/ipfs/QmUCqj5RysXZKiX5XJXMjTguqAAgWc8Ce3u6h2iXNbq4RX/"
+        });
+
+        nextTokenId = 1;
+    }
+
+    function mintMysteryBox() external payable {
+        uint256 mysteryBoxPrice = 0.005 ether;
+        require(msg.value >= mysteryBoxPrice, "Insufficient payment for mystery box");
+
+        uint8 typeId = getRandomType();
+        NFTType storage nftType = nftTypes[typeId];
+
+        require(nftType.currentSupply < nftType.maxSupply, "All NFTs of this type have been minted");
+
+        // Mint do token
+        uint8 tokenId = nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+        nftType.currentSupply += 1;
+        tokenType[tokenId] = typeId;
+
+        emit NFTCreated(tokenId, typeId, msg.sender);
+    }
+
+    function getRandomType() internal view returns (uint8) {
+        uint8 totalSupplyRemaining = 0;
+        for (uint8 i = 1; i <= 3; i++) {
+            totalSupplyRemaining += (nftTypes[i].maxSupply - nftTypes[i].currentSupply);
+        }
+
+        // Generate a number between 0 and (totalSupplyRemaining - 1)
+        uint256 randomValue = uint256(keccak256(abi.encodePacked(block.timestamp, block.prevrandao, msg.sender))) % totalSupplyRemaining;
+
+        uint8 cumulativeSum = 0;
+        for (uint8 i = 1; i <= 3; i++) {
+            uint8 supplyRemaining = nftTypes[i].maxSupply - nftTypes[i].currentSupply;
+            cumulativeSum += supplyRemaining;
+            
+            if (randomValue < cumulativeSum) {
+                return i;
+            }
+        }
+        revert FailedToFindAValidType();
+    }
+
+
+    function mint(uint8 typeId) external payable {
+        NFTType storage nftType = nftTypes[typeId];
+        require(nftType.maxSupply > 0, "NFT type does not exist");
+        require(nftType.currentSupply < nftType.maxSupply, "Max supply reached for this type");
+
+        // Verification of the payment
+        if (msg.value < nftType.mintPrice) revert InsufficientValue(nftType.mintPrice, msg.value);
+
+        // Token Mint
+        uint8 tokenId = nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+        nftType.currentSupply += 1;
+        tokenType[tokenId] = typeId;
+
+        emit NFTCreated(tokenId, typeId, msg.sender);
+    }
+
+    function setBaseURI(uint8 typeId, string memory baseURI) external onlyOwner {
+        nftTypes[typeId].baseURI = baseURI;
+    }
+
+    function setMintPrice(uint8 typeId, uint256 mintPrice) external onlyOwner {
+        nftTypes[typeId].mintPrice = mintPrice;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        uint8 typeId = tokenType[uint8(tokenId)];
+        NFTType storage nftType = nftTypes[typeId];
+        return string(abi.encodePacked(nftType.baseURI, nftTypeToTokenNumber(uint8(tokenId), typeId), ".json"));
+    }
+
+
+    function nftTypeToTokenNumber(uint8 tokenId, uint8 typeId) internal view returns (string memory) {
+        uint256 tokenNumber = tokenId - nftTypeStartingId(typeId);
+        return Strings.toString(tokenNumber + 1); // +1 to start with #1
+    }
+
+    function nftTypeStartingId(uint8 typeId) internal view returns (uint8) {
+        uint8 startingId = 1; 
+        for (uint8 i = 1; i < typeId; i++) {
+            startingId += nftTypes[i].maxSupply;
+        }
+        return startingId;
     }
 
     function withdraw() public onlyOwner {
-        // Trocar o transfer
         payable(owner()).transfer(address(this).balance);
     }
-
-    // Função para atualizar o contractURI se necessário
-    function setContractURI(string memory _contractURI) public onlyOwner {
-        contractURI = _contractURI;
-    }
-
-    function yoyaltyInfo(uint256 _tokenId, uint256 _salePrice) external view returns (address receiver, uint256 royaltyAmount) {
-        royaltyAmount = (_salePrice * royaltyFee) / 10000;
-        return (royaltyRecipient, royaltyAmount);
-    }
-
-    function setRoyaltyFee(uint16 _fee) public onlyOwner {
-        require(_fee <= 1000, "The max tax is 10%");
-        royaltyFee = _fee;
-    }
-
-    function burn(uint256 tokenId) public {
-        require(_isApprovedOrOwner(msg.sender, tokenId), "You dont have this token");
-        _burn(tokenId);
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721URIStorage, ERC721Enumerable) returns (bool) {
-        return super.supportsInterface(interfaceId);
-    }
-
 }
---
+++ generate_tokenURI.py
@@ -0,0 +1,103 @@
+import json
+import os
+
+# Função para gerar metadados JSON
+def create_metadata(name, description, image_url, position, jersey_number, age, club, overall_skill, rarity, num_units):
+    for i in range(1, num_units + 1):
+        # Nome exclusivo para cada unidade
+        metadata = {
+            "name": f"{name} - {rarity} #{i}",
+            "description": description,
+            "image": image_url,
+            "attributes": [
+                {
+                    "trait_type": "Position",
+                    "value": position
+                },
+                {
+                    "trait_type": "Jersey Number",
+                    "value": jersey_number
+                },
+                {
+                    "trait_type": "Age",
+                    "value": age
+                },
+                {
+                    "trait_type": "Club",
+                    "value": club
+                },
+                {
+                    "trait_type": "Overall Skill",
+                    "value": overall_skill
+                },
+                {
+                    "trait_type": "Rarity",
+                    "value": rarity
+                }
+            ]
+        }
+
+        # Criação da pasta baseada no nome do jogador
+        player_folder = name.replace(" ", "_").lower()
+        os.makedirs(player_folder, exist_ok=True)
+
+        # Salvando cada arquivo com seu número
+        filename = f"{player_folder}/{rarity.lower()}_{i}.json"
+        with open(filename, 'w') as f:
+            json.dump(metadata, f, indent=4)
+        print(f"Arquivo {filename} criado.")
+
+# Informações dos jogadores
+players = [
+    {
+        "name": "Luca Barbosa",
+        "description": "Exclusive NFT of forward João Silva in action for London FC.",
+        "image_url": "https://green-total-felidae-639.mypinata.cloud/ipfs/QmRnLzo8Qge62k5Cr9a7p6dZTZt3HtbzuXq8nHLu8wmec2",
+        "position": "Forward",
+        "jersey_number": 27,
+        "age": 25,
+        "club": "London FC",
+        "overall_skill": 90,
+        "rarity": "Legend",
+        "num_units": 10
+    },
+    {
+        "name": "Carlos Mendes",
+        "description": "Exclusive NFT of midfielder Carlos Mendes, known for his exceptional vision and passing.",
+        "image_url": "https://green-total-felidae-639.mypinata.cloud/ipfs/QmbovdUBWoutjUb7oXof7QrQDrW6XBykWwnFkYcWSvZ6BA",
+        "position": "Midfielder",
+        "jersey_number": 17,
+        "age": 28,
+        "club": "Paris City FC",
+        "overall_skill": 87,
+        "rarity": "Idol",
+        "num_units": 30
+    },
+    {
+        "name": "Roberto Alvarez",
+        "description": "Exclusive NFT of defender Roberto Alvarez, a stalwart in the backline for Milano FC.",
+        "image_url": "https://green-total-felidae-639.mypinata.cloud/ipfs/QmP86QowVR98iMBWTsYhbjrR1Xa3yj5MzHtnyKk1sBG75Z",
+        "position": "Defender",
+        "jersey_number": 4,
+        "age": 30,
+        "club": "Milano FC",
+        "overall_skill": 84,
+        "rarity": "Crack",
+        "num_units": 50
+    }
+]
+
+# Gerando arquivos JSON para cada jogador
+for player in players:
+    create_metadata(
+        player["name"],
+        player["description"],
+        player["image_url"],
+        player["position"],
+        player["jersey_number"],
+        player["age"],
+        player["club"],
+        player["overall_skill"],
+        player["rarity"],
+        player["num_units"]
+    )
